e007f4cb6763720bead67e9656d57e80d0ffac0c
Oscar Wos <theoscar@protonmail.com>
2018-01-07 22:02:00 +0000

    Removed tracking of bash scripts

--- Compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIRECTORY="/home/oscar/Git/BasePluginsPlus/"
-FILENAME="Base-Plugins-Plus"
-
-~/Git/SourceModScripting/spcomp $DIRECTORY/scripting/$FILENAME.sp -o$DIRECTORY/plugins/$FILENAME.smx
--- Upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIRECTORY="/home/oscar/Git/BasePluginsPlus/"
-
-git -C $DIRECTORY add --all
-git -C $DIRECTORY commit -a
-git -C $DIRECTORY push
